3059a453f6183859b131d04323d99c643653e307
Joshua Jinu <116093853+joshua-jinu@users.noreply.github.com>
2024-11-28 09:24:47 +0530

    Adding Blog page
    Axios Request to the backend MongoDB server

---
+++ frontend/src/components/Blogs.jsx
@@ -0,0 +1,54 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+function Blogs() {
+    const [blogs, setBlogs] = useState([]);
+    const [error, setError] = useState();
+
+    useEffect(()=>{
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('/api/blogs');
+                setBlogs(response.data.message);
+                setError(false);
+            }catch(err){
+                setError(err.message || "Error");
+            }
+        }
+
+        fetchData();
+    }, [])
+    
+    return (
+        <div>
+            {!error &&
+                blogs.map((blog)=>{
+                    return (
+                        <div key={blog.id} className='bg-slate-200 m-4 p-4 rounded-xl w-full flex justify-center items-center flex-col'>
+                            <div>
+                                <b>
+                                    <h1>{blog.title}</h1>
+                                </b>
+                            </div>
+                            <div>
+                                <i>
+                                    <h3>{blog.author}</h3>
+                                </i>
+                            </div>
+                            <br />
+                            <div>
+                                <h2>{blog.content}</h2>
+                            </div>
+                            <br />
+                            <br />
+                            <br />
+                        </div>
+                    )
+                })
+            }
+            
+        </div>
+    )
+}
+
+export default Blogs
